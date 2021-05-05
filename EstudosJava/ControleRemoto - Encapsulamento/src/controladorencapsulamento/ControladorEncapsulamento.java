20a2ecbf1b88ffd7bd99d97d8e5ea91ec8f1b7e7
Guilherme Ferreira <83267524+Guilhermeferreir@users.noreply.github.com>
2021-05-05 16:45:01 -0300

    commit control
    Commit do arquivo java controle remoto

---
+++ EstudosJava/ControleRemoto - Encapsulamento/src/controladorencapsulamento/ControladorEncapsulamento.java
@@ -0,0 +1,18 @@
+package controladorencapsulamento;
+
+
+public class ControladorEncapsulamento {
+
+  
+    public static void main(String[] args) {
+        ControleRemoto cr = new ControleRemoto();
+        
+        cr.desligar();
+        
+        
+        
+        
+       
+    }
+    
+}
