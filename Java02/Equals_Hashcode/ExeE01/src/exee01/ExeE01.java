7ce689bddce63d2a7dba0f68fc61b1633ce5cd7a
Guilherme Ferreira <83267524+Guilhermeferreir@users.noreply.github.com>
2021-05-22 04:33:27 -0300

    ExeE01

---
+++ Java02/Equals_Hashcode/ExeE01/src/exee01/ExeE01.java
@@ -0,0 +1,15 @@
+package exee01;
+
+
+public class ExeE01 {
+
+    
+    public static void main(String[] args) {
+        
+        Carro palio = new Carro("Palio");
+        Carro gol = new Carro("Gol");
+        
+        System.out.println(gol.equals(palio));
+    }
+    
+}
